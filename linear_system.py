f68c382ed9e29e2ab42a9c71eb31a87795f75324
Marcos Hideo Maruo <marcos.maruo@gmail.com>
2026-01-13 16:56:19 -0300

    Testing relative degree greater than one

--- linear_system.py
+++ linear_system.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import expm
 
 class linear_system:
     def __init__(self,b:np.ndarray,a:np.ndarray,sampling_rate:float):
@@ -63,4 +64,37 @@ class linear_system:
         
     def states(self):
         return self._X
+
+    def print_zoh(self):
+        print(f"A_d={expm(self._Ts*self._A)}")
+        print(f"B_d={np.linalg.solve(self._A, ((expm(self._Ts*self._A))-np.eye(self._N))@self._B)}")
+        print(f"C_d={self._C}")
+        print(f"D_d={self._D}")
+
     
+    def print_Euler(self):
+        print(f"A_d={(np.eye(self._N) + self._Ts*self._A)}")
+        print(f"B_d={self._Ts*self._B}")
+        print(f"C_d={self._C}")
+        print(f"D_d={self._D}")
+        print(f"\lambda = {np.linalg.eig(np.eye(self._N) + self._Ts*self._A)}")
+
+    def iterate_zoh(self, input_signal_sample):
+        self._X = expm(self._Ts*self._A)@self._X + np.linalg.solve(self._A, ((expm(self._Ts*self._A))-np.eye(self._N))@self._B)*[input_signal_sample]
+        y = self._C@self._X + self._D*input_signal_sample
+        return y
+
+    def simulate_zoh(self, input_signal, initial_conditions):
+        n_iter = len(input_signal)
+
+        state_history = np.zeros((self._N, n_iter))
+        output_history = np.zeros(n_iter)
+
+        self._X = initial_conditions
+        yold = 0
+        for i in range(n_iter):
+            state_history[:,i] = self._X
+            output_history[i] = yold 
+            yold = self.iterate_zoh(input_signal[i])
+            
+        return output_history, state_history
